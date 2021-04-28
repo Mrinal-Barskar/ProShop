e5e073fb8cc4c9a19bc763a42c7c713120f1a0c1
Mrinal-Techwondoe <79075798+Mrinal-Techwondoe@users.noreply.github.com>
2021-04-28 18:56:25 +0530

    Made some changes to configuration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,23 @@
+import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import HomeScreen from "./Screens/HomeScreen";
+import ProductScreen from "./Screens/ProductScreen";
+
 function App() {
   return (
-    <>
-      <h1>Welcome To ProShop</h1>
-    </>
+    <Router>
+      <Header></Header>
+      <main className="py-3">
+        <Container>
+          <Route path="/" component={HomeScreen} exact />
+          <Route path="/product/:id" component={ProductScreen} />
+        </Container>
+      </main>
+      <Footer></Footer>
+    </Router>
   );
 }
 
